da0e5338de88a9a0e84dced692065844552ce351
ab-style <WFB15376253289@outlook.com>
2023-09-06 19:53:31 +0800

    Perfect: Login UI and logic

--- src/authMiddleware.tsx
+++ src/authMiddleware.tsx
@@ -2,13 +2,14 @@ import React from "react";
 import {AuthContext} from "./hooks/useAuth";
 import {Suspense} from "react";
 import Login from "./components/Login";
+import {Spin} from "antd";
 
 export const Auth = (element: React.ReactNode) => {
     return <AuthContext.Consumer>
         {
             value =>
                 (
-                    <Suspense fallback={<h1>Loading...</h1>}>
+                    <Suspense fallback={<Spin size="large" />}>
                         {value.isAuthenticated ? element : <Login />}
                     </Suspense>
                 )
